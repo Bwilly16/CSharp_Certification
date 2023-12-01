3c44a49013915be1fd5b2fd8a2e8eba1cf04d082
Bwilly16 <benjamin.wilichowski@marquette.edu>
2023-12-01 15:25:04 -0600

    Inital Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C__Certification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing the output");
+        }
+    }
+}
