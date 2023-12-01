9b8afa3f367e0d2ee8765dff505aba9a9c87d52a
Bwilly16 <benjamin.wilichowski@marquette.edu>
2023-12-01 15:53:47 -0600

    Lesson 12 complete

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace C__Certification
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Testing the output");
+            double double_num = 0.0d;
+            float float_num = 5.5f;
+            decimal dec_num = 2.394m;
+
+            Console.WriteLine("Double: " + double_num + "\nFloat: " + float_num + "\nDecimal: " + dec_num);
+            
         }
     }
 }
